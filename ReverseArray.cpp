013252e13e33f9658bcbbc94926a66c6801dc036
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-06-23 20:44:26 +0530

    some changes

--- ReverseArray.cpp
+++ ReverseArray.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-/* Function to reverse arr[] from start to end*/
 void rvereseArray(int arr[], int start, int end)
 {
     while (start < end)
@@ -13,8 +12,7 @@ void rvereseArray(int arr[], int start, int end)
         end--;
     }
 }    
- 
-/* Utility function to print an array */
+
 void printArray(int arr[], int size)
 {
    for (int i = 0; i < size; i++)
@@ -23,22 +21,20 @@ void printArray(int arr[], int size)
    cout << endl;
 }
  
-/* Driver function to test above functions */
+
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6};
      
     int n = sizeof(arr) / sizeof(arr[0]);
  
-    // To print original array
+
     printArray(arr, n);
-     
-    // Function calling
+
     rvereseArray(arr, 0, n-1);
      
     cout << "Reversed array is" << endl;
      
-    // To print the Reversed array
     printArray(arr, n);
      
     return 0;
